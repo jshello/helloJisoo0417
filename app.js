1026f07d4df6f1efb81262a956877001007807d0
nemo nemo <bmxtest1012@gmail.com>
2017-04-17 01:01:06 -0500

    change3

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+ 
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+ 
+// create a new express server
+var app = express();
+ 
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+ 
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
  
